c891bbf4d27d55ef9d11b3bc81659e242d24c581
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2020-07-13 16:18:24 +0700

    Testing tool for author

---
+++ FOR-AUTHOR/testing.sh
@@ -0,0 +1,11 @@
+# Author script for testing package by temporary substituting installed production-ready package
+if [ `basename \`pwd\`` == "FOR-AUTHOR" ]; then
+	echo "[ERROR] 'cd ..' before running this"
+else
+	mv ~/.local/lib/python3.8/site-packages/np_chaonay ~/.local/lib/python3.8/site-packages/np_chaonay_orig~
+	ln -s ~/Projects/Applications/Python/Packages/np_chaonay/np_chaonay ~/.local/lib/python3.8/site-packages/np_chaonay
+	echo "Press enter to restore the original package..."
+	read -s
+	rm ~/.local/lib/python3.8/site-packages/np_chaonay
+	mv ~/.local/lib/python3.8/site-packages/np_chaonay_orig~ ~/.local/lib/python3.8/site-packages/np_chaonay
+fi
