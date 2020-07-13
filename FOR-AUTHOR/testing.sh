c825fa07ea124c41a7669d16764d712c84e54d81
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2020-07-13 17:10:14 +0700

    change words for another new development branch (this also called commit testing)

--- FOR-AUTHOR/testing.sh
+++ FOR-AUTHOR/testing.sh
@@ -3,7 +3,7 @@ if [ `basename \`pwd\`` == "FOR-AUTHOR" ]; then
 	echo "[ERROR] 'cd ..' before running this"
 else
 	mv ~/.local/lib/python3.8/site-packages/np_chaonay ~/.local/lib/python3.8/site-packages/np_chaonay_orig~
-	ln -s ~/Projects/Applications/Python/Packages/np_chaonay/np_chaonay ~/.local/lib/python3.8/site-packages/np_chaonay
+	ln -s ~/Projects/Applications/Python/Packages/np_chaonay_beta/np_chaonay ~/.local/lib/python3.8/site-packages/np_chaonay
 	echo "Press enter to restore the original package..."
 	read -s
 	rm ~/.local/lib/python3.8/site-packages/np_chaonay
