9ede42f81c5170181ef4aaae822bb90f9d77e704
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2020-09-04 22:42:27 +0700

    [components:np_chaonay.math] class added.

--- np_chaonay/math.py
+++ np_chaonay/math.py
@@ -1,7 +1,7 @@
 # Name: NP-chaonay/Math Python module
 # Description: Module contains object for mathematics usage
 # Author: NP-chaonay (Nuttapong Punpipat)
-# Version: V.1.0.3_dev1
+# Version: V.1.(0).3_dev1
 # Version Note: 
 #       - Major version: indicates of very significant changes or changes that break compatibility on some system/platforms.
 #       - Minor version: indicates of significant changes or features adding.
@@ -18,6 +18,7 @@ Module contains object for mathematics usage
 Module Contents (excluding _*) :
 - prime_generator (function)
 - polymonial_long_division (function)
+- Vector (class)
 """
 
 ######## Initialization ########
@@ -90,3 +91,111 @@ def polymonial_long_division(num_ce,den_ce):
 		if i+1==round_num: break
 		else: rmd_ce=np.concatenate((rmd_ce[1:],(num_ce[i+len(rmd_ce)],)))
 	return result_ce,rmd_ce[1:]
+
+class Vector():
+	# QualityCheckTags:
+	# [/] CODE
+	# [/] INPUTCHECK
+	# [/] DOCS
+	# [/] TEST
+	''' Vector implementation
+	
+	# Object Creation
+	    Vector(*dimensions)
+	  Optional Arguments:
+	  - *dimensions (int,float): Value of each dimensions
+	
+	# Object Representation
+	  >>> Vector(-1,0,1)
+	  Vector((-1,0,1)
+	  
+	# Object Iteration
+	  >>> list(Vector(-1,0,1)
+	  [-1,0,1]
+	
+	# Negative Unary
+	  >>> -Vector(-1,0,1)
+	  Vector(1,0,-1)
+	
+	# Adding/Subtracting
+	  ## With same type
+	    >>> Vector(-1,0,1)+Vector(-1,0,1)
+	    Vector(-2,0,2)
+	    >>> Vector(-1,0,1)-Vector(-1,0,1)
+	    Vector(0,0,0)
+	  ## With int-alike/float-alike
+	    >>> Vector(-1,0,1)+1
+	    Vector(0,1,2)
+	    >>> Vector(-1,0,1)-1
+	    Vector(-2,-1,0)
+	
+	# Multiplying
+	  ## With int-alike/float-alike
+	    >>> Vector(-1,0,1)*10
+	    Vector(-10,0,10)
+	
+	# Methods
+	  - magnitude()
+	    Returns: Magnitude of vector
+	
+	# Variables
+	  - dimensions (list) : consists of dimensions' values
+	'''
+	dimensions=[]
+	def __init__(self,*dimensions):
+		# Type Checking
+		for dimension in dimensions: _npc_m.alternative_isinstance('*dimensions',(int,float),dimension)
+		self.dimensions=list(dimensions)
+	def __repr__(self):
+		return 'Vector('+repr(self.dimensions)[1:-1]+')'
+	def __iter__(self):
+		return (dimension for dimension in self.dimensions)
+	def __neg__(self):
+		new_dimensions=[]
+		for i in range(len(self.dimensions)):
+			new_dimensions+=[-self.dimensions[i]]
+		return type(self)(*new_dimensions)
+	def __add__(self,another):
+		if type(self)==type(another):
+			if len(self.dimensions)==len(another.dimensions):
+				new_dimensions=[]
+				for i in range(len(self.dimensions)):
+					new_dimensions+=[self.dimensions[i]+another.dimensions[i]]
+				return type(self)(*new_dimensions)
+			else:
+				raise ValueError('Adding vectors with different dimensions.')
+		else:
+			# Type Checking (Adding type(self) for exception displaying
+			_npc_m.alternative_isinstance('adding value',(int,float,type(self)),another)
+			new_dimensions=[]
+			for i in range(len(self.dimensions)):
+				new_dimensions+=[self.dimensions[i]+another]
+			return type(self)(*new_dimensions)
+	def __sub__(self,another):
+		if type(self)==type(another):
+			if len(self.dimensions)==len(another.dimensions):
+				new_dimensions=[]
+				for i in range(len(self.dimensions)):
+					new_dimensions+=[self.dimensions[i]-another.dimensions[i]]
+				return type(self)(*new_dimensions)
+			else:
+				raise ValueError('Subtracting vectors with different dimensions.')
+		else:
+			# Type Checking (Adding type(self) for exception displaying
+			_npc_m.alternative_isinstance('adding value',(int,float,type(self)),another)
+			new_dimensions=[]
+			for i in range(len(self.dimensions)):
+				new_dimensions+=[self.dimensions[i]-another]
+			return type(self)(*new_dimensions)
+	def __mul__(self,another):
+		if type(self)==type(another):
+			raise ValueError('Muliplying vectors is not implemented yet.')
+		else:
+			# Type Checking
+			_npc_m.alternative_isinstance('adding value',(int,float),another)
+			new_dimensions=[]
+			for i in range(len(self.dimensions)):
+				new_dimensions+=[self.dimensions[i]*another]
+			return type(self)(*new_dimensions)
+	def magnitude(self):
+		return (sum(tuple(map(lambda x: x**2,self.dimensions))))**0.5
