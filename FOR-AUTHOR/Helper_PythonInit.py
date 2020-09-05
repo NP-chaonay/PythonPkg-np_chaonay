618883338694a21a7bbaec13e25dae6007659d17
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2020-09-05 17:31:45 +0700

    [FOR_AUTHOR] Files changed

--- FOR-AUTHOR/Helper_PythonInit.py
+++ FOR-AUTHOR/Helper_PythonInit.py
@@ -29,24 +29,24 @@ def SW_new_uploading_tag(version,development=False,commit=None):
 	tag_desc='Latest commit of version \''+version+'\''
 	new_uploading_tag(tag_name,tag_desc,commit)
 
-def SW_diff(path):
-	sp.run(['git', 'diff', 'origin/master', 'origin/development', '--', path])
+def SW_diff(path,a='origin/master',b='origin/development'):
+	sp.run(['git', 'diff', a, b, '--', path])
 
 def SW_git_push(msg):
-	if not msg: print_categorical_bracket('git_push:ERROR','GIT push function requires committing message.')
-	print_categorical_bracket('git_push','GIT Add')
-	sp=sp.run(['git', 'add', '.'])
-	if sp.returncode!=0:
-		print_categorical_bracket('git_push:ERROR','GIT Add Error')
-	print_categorical_bracket('git_push','GIT Commit')
-	sp=sp.run(['git', 'commit', '-m', msg])
-	if sp.returncode!=0:
-		print_categorical_bracket('git_push:ERROR','GIT Commit Error, undoing changes staging.')
+	if not msg: npc_m.print_categorical_bracket('git_push:ERROR','GIT push function requires committing message.')
+	npc_m.print_categorical_bracket('git_push','GIT Add')
+	sp_log=sp.run(['git', 'add', '.'])
+	if sp_log.returncode!=0:
+		npc_m.print_categorical_bracket('git_push:ERROR','GIT Add Error')
+	npc_m.print_categorical_bracket('git_push','GIT Commit')
+	sp_log=sp.run(['git', 'commit', '-m', msg])
+	if sp_log.returncode!=0:
+		npc_m.print_categorical_bracket('git_push:ERROR','GIT Commit Error, undoing changes staging.')
 		sp.run(['git', 'reset'])
-	print_categorical_bracket('git_push','GIT Push')
+	npc_m.print_categorical_bracket('git_push','GIT Push')
 	sp.run(['python3', npc_m.slash_suffix_for_dir(npc_m.get_homedir())+".password/ ", 'github'])	
-	sp=sp.run(['git', 'push'])
-	if sp.returncode!=0:
-		print_categorical_bracket('git_push:ERROR','GIT Push Error, cancelling commit.')
+	sp_log=sp.run(['git', 'push'])
+	if sp_log.returncode!=0:
+		npc_m.print_categorical_bracket('git_push:ERROR','GIT Push Error, cancelling commit.')
 		sp.run(['git', 'reset', '--soft', 'HEAD^'])
 		sp.run(['git', 'reset'])
