f38ce860fb76e81138c971f77e24641907b7ad64
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2020-09-05 15:40:50 +0700

    [FOR-AUTHOR] Files about testings Added/Changed.]

--- FOR-AUTHOR/testing.sh
+++ FOR-AUTHOR/testing.sh
@@ -1,11 +1,7 @@
 # Author script for testing package by temporary substituting installed production-ready package
-if [ `basename \`pwd\`` == "FOR-AUTHOR" ]; then
-	echo "[ERROR] 'cd ..' before running this"
+if [ `basename \`pwd\`` == "PythonPkg-np_chaonay" ]; then
+	echo "[ERROR] Change directory to root, or check the root directory name to 'PythonPkg-np_chaonay'"
 else
 	mv ~/.local/lib/python3.8/site-packages/np_chaonay ~/.local/lib/python3.8/site-packages/np_chaonay_orig~
-	ln -s ~/Projects/Applications/Python/Packages/np_chaonay_beta/np_chaonay ~/.local/lib/python3.8/site-packages/np_chaonay
-	echo "Press enter to restore the original package..."
-	read -s
-	rm ~/.local/lib/python3.8/site-packages/np_chaonay
-	mv ~/.local/lib/python3.8/site-packages/np_chaonay_orig~ ~/.local/lib/python3.8/site-packages/np_chaonay
+	ln -s ./np_chaonay ~/.local/lib/python3.8/site-packages/np_chaonay
 fi
---
+++ FOR-AUTHOR/undo-testing.sh
@@ -0,0 +1,3 @@
+# Undo actions of testing.sh
+rm ~/.local/lib/python3.8/site-packages/np_chaonay
+mv ~/.local/lib/python3.8/site-packages/np_chaonay_orig~ ~/.local/lib/python3.8/site-packages/np_chaonay
