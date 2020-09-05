c5a12370e1eab756cd9de9a8de06d50e62ce209e
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2020-09-05 18:10:28 +0700

    [FOR_AUTHOR] File changed

--- FOR-AUTHOR/pypi-update.sh
+++ FOR-AUTHOR/pypi-update.sh
@@ -1,6 +1,6 @@
 # Author script for uploading package (Do not run this script 
-if [ `basename \`pwd\`` == "FOR-AUTHOR" ]; then
-	echo "[ERROR] 'cd ..' before running this"
+if [ `basename \`pwd\`` != "PythonPkg-np_chaonay" ]; then
+	echo "[ERROR] Change directory to root, or check the root directory name to 'PythonPkg-np_chaonay'"
 else
 	rm dist/*
 	python3 setup.py sdist bdist_wheel
