f2d5dfc1fa480e416a4dc333676fa4c4a7d659c3
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2020-09-05 16:49:03 +0700

    [FOR_AUTHOR] Files changed

--- FOR-AUTHOR/testing.sh
+++ FOR-AUTHOR/testing.sh
@@ -1,5 +1,5 @@
 # Author script for testing package by temporary substituting installed production-ready package
-if [ `basename \`pwd\`` == "PythonPkg-np_chaonay" ]; then
+if [ `basename \`pwd\`` != "PythonPkg-np_chaonay" ]; then
 	echo "[ERROR] Change directory to root, or check the root directory name to 'PythonPkg-np_chaonay'"
 else
 	mv ~/.local/lib/python3.8/site-packages/np_chaonay ~/.local/lib/python3.8/site-packages/np_chaonay_orig~
