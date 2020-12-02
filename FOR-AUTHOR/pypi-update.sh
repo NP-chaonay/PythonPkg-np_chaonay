725b4e68e0530d46f720c1e942975a4f13683f1c
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2020-12-02 17:30:33 +0700

    (Typo Fixing) on (FOR AUTHOR) about (Path name edited) : -

--- FOR-AUTHOR/pypi-update.sh
+++ FOR-AUTHOR/pypi-update.sh
@@ -1,6 +1,6 @@
 # Author script for uploading package (Do not run this script 
-if [ `basename \`pwd\`` != "PythonPkg-np_chaonay" ]; then
-	echo "[ERROR] Change directory to root, or check the root directory name to 'PythonPkg-np_chaonay'"
+if [ `basename \`pwd\`` != "np_chaonay_master" ]; then
+	echo "[ERROR] Change directory to root, or check the root directory name to 'np_chaonay_master'"
 else
 	rm dist/*
 	python3 setup.py sdist bdist_wheel
--- FOR-AUTHOR/testing.sh
+++ FOR-AUTHOR/testing.sh
@@ -1,6 +1,6 @@
 # Author script for testing package by temporary substituting installed production-ready package
-if [ `basename \`pwd\`` != "PythonPkg-np_chaonay" ]; then
-	echo "[ERROR] Change directory to root, or check the root directory name to 'PythonPkg-np_chaonay'"
+if [ `basename \`pwd\`` != "np_chaonay_master" ]; then
+	echo "[ERROR] Change directory to root, or check the root directory name to 'np_chaonay_master'"
 else
 	mv ~/.local/lib/python3.8/site-packages/np_chaonay ~/.local/lib/python3.8/site-packages/np_chaonay_orig~
 	ln -s ./np_chaonay ~/.local/lib/python3.8/site-packages/np_chaonay
