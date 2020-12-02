84788816e4458948a41521557472ffa9f8170f03
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2020-12-02 17:27:56 +0700

    (Typo Fixing) on (FOR AUTHOR) about (Path name edited) : -

--- FOR-AUTHOR/pypi-update.sh
+++ FOR-AUTHOR/pypi-update.sh
@@ -1,6 +1,6 @@
 # Author script for uploading package (Do not run this script 
-if [ `basename \`pwd\`` != "PythonPkg-np_chaonay" ]; then
-	echo "[ERROR] Change directory to root, or check the root directory name to 'PythonPkg-np_chaonay'"
+if [ `basename \`pwd\`` != "np_chaonay_dev" ]; then
+	echo "[ERROR] Change directory to root, or check the root directory name to 'np_chaonay_dev'"
 else
 	rm dist/*
 	python3 setup.py sdist bdist_wheel
